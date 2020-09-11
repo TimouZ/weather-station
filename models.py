6cb174b6d2ae6db89190c4fbda4f22a1188a1946
tim <22520614+TimouZ@users.noreply.github.com>
2020-09-11 13:25:58 +0300

    Change in data model

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
+"""Data models.
 """
 
-"""
 import datetime
-from peewee import (Model, SqliteDatabase, DateField, FloatField, ForeignKeyField, DateTimeField, datetime as peewee_datetime)
+from peewee import (Model, SqliteDatabase, FloatField, ForeignKeyField, DateTimeField, datetime as peewee_datetime)
 from config import DATABASE_NAME
 
 database = SqliteDatabase(DATABASE_NAME)  # Only for development
@@ -15,9 +15,7 @@ class _BaseModel(Model):
 
 
 class TemperatureLocal(_BaseModel):
-    """Table for storing temperature data from local source"""
-
-
+    """Stores temperature data from local source"""
     date_time = DateTimeField(formats="%d-%m-%Y")
     temperature = FloatField()
     temperature_min = FloatField()
@@ -25,8 +23,7 @@ class TemperatureLocal(_BaseModel):
 
 
 class PressureLocal(_BaseModel):
-    """Table for storing pressure data from local source"""
-
+    """Stores pressure data from local source"""
     date_time = DateTimeField(formats="%d-%m-%Y")
     pressure = FloatField()
     pressure_min = FloatField()
@@ -34,8 +31,7 @@ class PressureLocal(_BaseModel):
 
 
 class HumidityLocal(_BaseModel):
-    """Table for storing humidity data from local source"""
-
+    """Stores humidity data from local source"""
     date_time = DateTimeField(formats="%d-%m-%Y")
     humidity = FloatField()
     humidity_min = FloatField()
@@ -43,8 +39,7 @@ class HumidityLocal(_BaseModel):
 
 
 class TemperatureApi1(_BaseModel):
-    """Table for storing temperature data from remote API source"""
-
+    """Stores temperature data from remote API source"""
     date_time = DateTimeField(formats="%d-%m-%Y")
     temperature = FloatField()
     temperature_min = FloatField()
@@ -52,8 +47,7 @@ class TemperatureApi1(_BaseModel):
 
 
 class PressureApi1(_BaseModel):
-    """Table for storing pressure data from remote API source"""
-
+    """Stores pressure data from remote API source"""
     date_time = DateTimeField(formats="%d-%m-%Y")
     pressure = FloatField()
     pressure_min = FloatField()
@@ -61,46 +55,45 @@ class PressureApi1(_BaseModel):
 
 
 class HumidityApi1(_BaseModel):
-    """Table for storing humidity data from remote API source"""
-
+    """Stores humidity data from remote API source"""
     date_time = DateTimeField(formats="%d-%m-%Y")
     humidity = FloatField()
     humidity_min = FloatField()
     humidity_max = FloatField()
 
-    def to_dict(self):
-        return self._data
 
-
-class WeatherStation(_BaseModel):
-    """Summary table for aggregating data from all sources"""
-
-    date_time = DateTimeField(formats="%d-%m-%Y")
+class SummaryTemperature(_BaseModel):
     temperature_local = ForeignKeyField(TemperatureLocal)
     temperature_api1 = ForeignKeyField(TemperatureApi1)
+
+
+class SummaryPressure(_BaseModel):
     pressure_local = ForeignKeyField(PressureLocal)
     pressure_api1 = ForeignKeyField(PressureApi1)
+
+
+class SummaryHumidity(_BaseModel):
     humidity_local = ForeignKeyField(HumidityLocal)
     humidity_api1 = ForeignKeyField(HumidityApi1)
+
 
 def init_db():
     """Tables creation and test data initiation function"""
     with database:
         print("Drop tables...")
         database.drop_tables([TemperatureLocal, TemperatureApi1, PressureLocal,
-                              PressureApi1, HumidityLocal, HumidityApi1, WeatherStation])
+                              PressureApi1, HumidityLocal, HumidityApi1])
         print("Done...")
         print("Create tables...")
         database.create_tables([TemperatureLocal, TemperatureApi1, PressureLocal,
-                              PressureApi1, HumidityLocal, HumidityApi1, WeatherStation])
+                              PressureApi1, HumidityLocal, HumidityApi1])
         print("Done")
         print("Adding test data")
         TemperatureLocal.create(date_time="18-05-2020", temperature=25.34, temperature_min=25.56, temperature_max=26.33)
         PressureLocal.create(date_time="18-05-2020", pressure=25.34, pressure_min=25.56, pressure_max=26.33)
         HumidityLocal.create(date_time="18-05-2020", humidity=25.34, humidity_min=25.56, humidity_max=26.33)
-        TemperatureApi1.create(date_time="18-05-2020", temperature=25.34, temperature_min=25.56, temperature_max=26.33)
+        TemperatureApi1.create(date_time="18-05-2020", temperature=25.34, temperature=25.56, temperature=26.33)
         PressureApi1.create(date_time="18-05-2020", pressure=25.34, pressure_min=25.56, pressure_max=26.33)
         HumidityApi1.create(date_time="18-05-2020", humidity=25.34, humidity_min=25.56, humidity_max=26.33)
         print("Done")
 
-init_db()

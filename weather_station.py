9cfcba504eabc0fff66b359547a06277b01c495d
tim <22520614+TimouZ@users.noreply.github.com>
2020-09-14 15:50:49 +0300

    Refactoring config load. PEP8

--- weather_station.py
+++ weather_station.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 import os
-import flask
+from flask import Flask
+from helpers import init_db
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+init_db()
+print("Init state complete")
+
+import views
